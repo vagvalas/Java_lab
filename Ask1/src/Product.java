f43fe558225130a035d3021c62c55cd3b8db6d00
vaggosval <vaggosval@195-251-211-242.uom.gr>
2019-10-11 10:02:25 +0300

    New class on Ask1

---
+++ Ask1/src/Product.java
@@ -0,0 +1,15 @@
+
+public class Product {
+	//IDIOTITES - PEDIA - METAVLITES STIGMIOTYPOU
+private String name;
+private double price;
+private int score;
+
+//kataskeuastis
+public Product()
+{
+	name = "";
+	price = 1;
+	score = 0;
+	}
+}
